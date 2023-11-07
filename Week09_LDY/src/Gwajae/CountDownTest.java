6997e1869f554759b35e5779c4675452ee04bfe1
Dayxxn343 <144751811+Dayxxn343@users.noreply.github.com>
2023-11-07 11:34:40 +0900

    Add files via upload

---
+++ Week09_LDY/src/Gwajae/CountDownTest.java
@@ -0,0 +1,75 @@
+package Gwajae;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class CountDownTest extends JFrame {
+    private JLabel label;
+    private Thread t;
+
+    class Counter extends Thread {
+        private boolean counting;
+
+        public void run() {
+            for (int i = 0; i <= 10; i++) {
+                if (!counting) {
+                    return;
+                }
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    return;
+                }
+                label.setText(i + "");
+            }
+        }
+    }
+
+    public CountDownTest() {
+        setTitle("카운트다운");
+        setSize(400, 150);
+        getContentPane().setLayout(null);
+
+        label = new JLabel("0");
+        label.setBounds(0, 0, 384, 111);
+        label.setFont(new Font("Serif", Font.BOLD, 100));
+        getContentPane().add(label);
+
+        JButton stopButton = new JButton("카운트 중지");
+        stopButton.setBounds(247, 25, 125, 23);
+        getContentPane().add(stopButton);
+
+        JButton startButton = new JButton("카운트 다시 시작");
+        startButton.setBounds(247, 60, 125, 23);
+        getContentPane().add(startButton);
+
+        stopButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (t != null) {
+                    t.interrupt();
+                }
+            }
+        });
+
+        startButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                t = new Counter();
+                t.counting = true; // 카운트 시작 플래그 설정
+                t.start();
+            }
+        });
+
+        setVisible(true);
+    }
+
+	public static void main(String[] args) {
+        CountDownTest t = new CountDownTest();
+        t.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
